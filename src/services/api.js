648dcc4d7c5ac0b7dbf635882f44913142517967
fsmith01539 <josearthurferreiracardoso@gmail.com>
2026-01-12 22:36:51 -0300

    Colocando a api do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
+  baseURL: import.meta.env.VITE_API_URL || 'https://backend-alumni-five.vercel.app/',
 });
 
 // Toda request manda o token (se existir)
